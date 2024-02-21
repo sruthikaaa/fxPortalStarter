156e1518ce9eb6ffa84721427e56b8a4603194d0
sruthikaaa <160781812+sruthikaaa@users.noreply.github.com>
2024-02-22 01:01:35 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,18 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
+// but useful for running the script in a standalone fashion through node <script>.
 //
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// You can also run a script with npx hardhat run <script>. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
 
-  const token = await hre.ethers.deployContract("MetaToken");
+  const City_pods = await hre.ethers.getContractFactory("Pods");
+  const pods=await City_pods.deploy();
+  await pods.deployed();
 
-  console.log("Token address:", await token.getAddress());
+  console.log("Your contract address=", pods.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
